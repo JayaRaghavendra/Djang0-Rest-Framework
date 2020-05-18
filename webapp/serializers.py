4d0e48b9b9055bd885bf56580ac1dee0ec0cb1a7
Jaya Raghavendra <jay.raghav333@gmail.com>
2020-05-19 05:11:38 +0530

    Add files via upload

---
+++ webapp/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers, fields
+from .models import *
+#from . models import Facility, Floor , Sector
+
+class SectorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Sector
+        fields = '__all__'
+
+class FloorSerializer(serializers.ModelSerializer):
+
+    floor_secotrs = SectorSerializer(read_only=True, many=True)    
+
+    class Meta:
+        model = Floor
+        fields = ('floor_id','floor_no','floor_name','floor_secotrs')
+
+class FacilitySerializer(serializers.ModelSerializer):
+    facility_floors = FloorSerializer(read_only=True,many=True) 
+    
+    class Meta:
+        model = Facility
+        fields = ('id','Facility_id','name','location','city','country','facility_floors')
+
